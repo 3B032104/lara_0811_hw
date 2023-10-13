8fd72f3c6ebcc3c4cc39b2f974414a33a1d971f5
3B032104 <3b032104@gm.student.ncut.edu.tw>
2023-10-13 20:13:05 +0800

    Add route home,about,news

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route Home
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// Route about
+Route::get('about', [aboutController::class, 'index'])->name('about.index');
+
+// Route news
+Route::get('news', [newsController::class, 'index'])->name('news.index');
